3cc7a6a9dd521793ce01c3fa7ccd28f8420d1cdd
Jarmo Lindström <yaramodesu@gmail.com>
2023-01-18 08:07:23 +0200

    tähän asti kaikki toimii

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame, sys
+from player import  Player
 
 class Game:
     def __init__(self):
-        pass
+        player_sprite = Player((screen_width / 2, screen_height), 600, 5)
+        self.player = pygame.sprite.GroupSingle(player_sprite)
+
     def run(self):
-        pass
+        self.player.update()
+        self.player.draw(screen)
 
 if __name__ == '__main__':
     screen_width = 600
