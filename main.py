c512df0b9e619cd0342d91d15b68c056d5ecd4bb
Jarmo Lindström <yaramodesu@gmail.com>
2023-01-20 09:20:10 +0200

    yksi block, este tehty

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import pygame, sys
 from player import  Player
+import obsticle
+
 
 class Game:
     def __init__(self):
+        #player setup
         player_sprite = Player((screen_width / 2, screen_height), 600, 5)
         self.player = pygame.sprite.GroupSingle(player_sprite)
+
+        #obsticle setup
+        self.shape = obsticle.shape
+        self.block_size = 6
+        self.blocks = self.blocks = pygame.sprite.Group()
+        self.create_obstacle()
+    def create_obstacle(self):
+         for row_index, row in enumerate(self.shape):
+             for col_index, col in enumerate(row):
+                 if col == 'x':
+                    x = col_index * self.block_size
+                    y = row_index * self.block_size
+                    block = obsticle.Block(self.block_size,(241,79,80),x,y)
+                    self.blocks.add(block)
 
     def run(self):
         self.player.update()
@@ -12,6 +29,7 @@ class Game:
         self.player.sprite.lasers.draw(screen)
 
         self.player.draw(screen)
+        self.blocks.draw(screen)
 
 if __name__ == '__main__':
     screen_width = 600
