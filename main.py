38e4e864a85deeeedc11de068994e127e44c1269
Jarmo Lindström <yaramodesu@gmail.com>
2023-01-24 09:14:19 +0200

    virhe oli alie.py. y ja x unohtunut sieltä

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame, sys
 from player import  Player
 import obsticle
-
+from alien import Alien
 
 class Game:
     def __init__(self):
@@ -16,6 +16,12 @@ class Game:
         self.obstacle_amount = 4
         self.obstacle_x_positions = [num * (screen_width / self.obstacle_amount) for num in range(self.obstacle_amount)]
         self.create_multible_obsticles(*self.obstacle_x_positions,x_start = screen_width / 15, y_start =480)
+
+        #alien setup
+        self.aliens = pygame.sprite.Group()
+        self.alien_setup(rows =6, cols=8)
+        self.alien_direction = 1
+
     def create_obstacle(self, x_start, y_start, offset_x):
          for row_index, row in enumerate(self.shape):
              for col_index, col in enumerate(row):
@@ -28,13 +34,30 @@ class Game:
     def create_multible_obsticles(self, *offset, x_start, y_start):
         for offset_x in offset:
             self.create_obstacle(x_start,y_start,offset_x)
+
+    def alien_setup(self, rows,cols, x_distance = 60, y_distance = 48, x_offset = 70, y_offset = 100):
+        for row_index, row in enumerate(range(rows)):
+            for col_index, col in enumerate(range(cols)):
+                x = col_index * x_distance +x_offset
+                y = row_index * y_distance +y_offset
+
+                if row_index == 0: alien_sprite = Alien('red',x,y)
+                elif 1 <= row_index <=2: alien_sprite = Alien('green', x,y)
+                else: alien_sprite = Alien('yellow',x,y)
+
+                self.aliens.add(alien_sprite)
+
+    def alien_position_checker(self):
+        all_aliens
     def run(self):
         self.player.update()
+        self.aliens.update(self.alien_direction)
 
         self.player.sprite.lasers.draw(screen)
-
         self.player.draw(screen)
+
         self.blocks.draw(screen)
+        self.aliens.draw(screen)
 
 if __name__ == '__main__':
     screen_width = 600
